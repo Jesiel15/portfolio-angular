1e76d29a0cb2ae8b657b0e3b58fd11841c8ffe7e
Jesiel Faria <fariajesiel@gmail.com>
2025-03-21 11:03:22 -0300

    Implementando pages contatos

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-contato',
   standalone: false,
   templateUrl: './contato.component.html',
-  styleUrl: './contato.component.scss'
+  styleUrl: './contato.component.scss',
 })
 export class ContatoComponent {
-
+  email = 'fariajesiel@gmail.com';
+  telefone = '(61) 9 8189-8269';
 }
