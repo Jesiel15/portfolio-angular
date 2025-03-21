86e001044c47c955119c271fd345a1211d176de2
Jesiel Faria <fariajesiel@gmail.com>
2025-03-21 12:01:54 -0300

    Implementando redes sociais

--- src/app/pages/redes-sociais/redes-sociais.component.ts
+++ src/app/pages/redes-sociais/redes-sociais.component.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-redes-sociais',
   standalone: false,
   templateUrl: './redes-sociais.component.html',
-  styleUrl: './redes-sociais.component.scss'
+  styleUrl: './redes-sociais.component.scss',
 })
 export class RedesSociaisComponent {
+  // Função de navegação para seções específicas
+  onNavigate(event: MouseEvent, section: string): void {
+    event.preventDefault(); // Impede o comportamento padrão de navegação
 
+    const element = document.getElementById(section);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
