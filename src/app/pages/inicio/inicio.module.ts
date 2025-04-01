62b5c58775fe26ed27fb033d2b00b5ca7f459741
Jesiel Faria <fariajesiel@gmail.com>
2025-04-01 16:02:00 -0300

    Implementando background animado com imagem

--- src/app/pages/inicio/inicio.module.ts
+++ src/app/pages/inicio/inicio.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './inicio.component';
 import { ApresentacaoModule } from "../../components/apresentacao/apresentacao.module";
+import { BackgroundModule } from "../../components/background/background.module";
 
 @NgModule({
   declarations: [InicioComponent],
-  imports: [CommonModule, ApresentacaoModule],
+  imports: [CommonModule, ApresentacaoModule, BackgroundModule],
   exports: [InicioComponent],
 })
 export class InicioModule {}
