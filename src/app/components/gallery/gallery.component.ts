6c23401fac6236ea0f7596d95d9d93886a500189
Jesiel Faria <fariajesiel@gmail.com>
2025-04-07 10:55:53 -0300

    Implementando projeto na galeria do desafio

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -14,31 +14,37 @@ export class GalleryComponent implements OnInit {
         'Projeto criado para ideias de heróis com seus poderes e características.',
       image: 'assets/superheros/superheros1.png',
       category: 'Fullstack',
+      tecnologia: 'Angular, Node Js e AWS EC2',
     },
     {
       title: 'Vejoflix',
       description: 'Projeto criado para salvar vídeos por categorias.',
       image: 'assets/vejoflix/vejoflix2.png',
       category: 'Fullstack',
+      tecnologia: 'React js, Node Js e AWS EC2',
     },
     {
-      title: 'Projeto 3',
-      description: 'Descrição do projeto 3.',
-      image: 'assets/exemplos/3.jpg',
-
-      category: 'Front',
+      title: 'Desafio 7 dias',
+      description:
+        'Desafio técnico realizado em uma semana para uma entrevista de emprego.',
+      image: 'assets/act_challenger/home.png',
+      category: 'Fullstack',
+      tecnologia:
+        'Angular, Node JS, PHP, Dockers, Postman, AWS EC2, e Testes unitários.',
     },
     {
       title: 'Projeto 4',
       description: 'Descrição do projeto 4.',
       image: 'assets/exemplos/5.jpg',
       category: 'Web',
+      tecnologia: '',
     },
     {
       title: 'Projeto 5',
       description: 'Descrição do projeto 5.',
       image: 'assets/exemplos/5.jpg',
       category: 'Web',
+      tecnologia: '',
     },
     {
       title: 'Projeto 6',
@@ -46,18 +52,21 @@ export class GalleryComponent implements OnInit {
       image: 'assets/exemplos/6.jpg',
 
       category: 'Back',
+      tecnologia: '',
     },
     {
       title: 'Projeto 7',
       description: 'Descrição do projeto 7.',
       image: 'assets/exemplos/7.jpg',
       category: 'Back',
+      tecnologia: '',
     },
     {
       title: 'Projeto 8',
       description: 'Descrição do projeto 8.',
       image: 'assets/exemplos/7.jpg',
       category: 'Back',
+      tecnologia: '',
     },
     {
       title: 'Projeto 9',
@@ -65,13 +74,15 @@ export class GalleryComponent implements OnInit {
       image: 'assets/exemplos/9.jpg',
 
       category: 'Web',
+      tecnologia: '',
     },
     {
       title: 'Projeto 8',
       description: 'Descrição do projeto 8.',
       image: 'assets/exemplos/7.jpg',
       category: 'Web',
-    }
+      tecnologia: '',
+    },
   ];
 
   filteredProjects = this.projects; // Inicialmente, exibe todos os projetos
