27be8246f35277abb449772b1447198fc2f5f883
Jesiel Faria <fariajesiel@gmail.com>
2025-04-07 11:03:33 -0300

    Ajuste no estilo dos cards na galeria

--- src/app/components/card-gallery/card-gallery.component.ts
+++ src/app/components/card-gallery/card-gallery.component.ts
@@ -7,16 +7,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card-gallery.component.scss',
 })
 export class CardGalleryComponent {
-  @Input() day: string = '13';
-  @Input() month: string = 'Aug';
-  @Input() year: string = '2016';
-  @Input() author: string = 'Jane Doe';
-  @Input() title: string = 'Stranger Things: The sound of the Upside Down';
-  @Input() text: string =
-    'The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.';
-  @Input() likes: number = 18;
-  @Input() comments: number = 3;
-  @Input() img: string = 'test';
+  @Input() day: string = '';
+  @Input() month: string = '';
+  @Input() year: string = '';
+  @Input() author: string = 'Projeto';
+  @Input() title: string = '';
+  @Input() text: string = '';
+  @Input() likes: number = 0;
+  @Input() comments: number = 0;
+  @Input() img: string = '';
 
   // Método para obter o estilo do background
   getBackgroundStyle() {
